5714504fa7f3122a9a8cd3dfd90ca60c303e36e6
Erodri6z <erikrodriguez2417@gmail.com>
2023-11-02 14:18:55 -0500

    solve another coding challange

--- codewars.py
+++ codewars.py
@@ -7,3 +7,5 @@ def high_and_low(n):
   print(str(final_arr[-1]) + "," + str(final_arr[0]))
 
 high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4")
+
+
---
+++ javascript.js
@@ -0,0 +1,8 @@
+function moveZeros(arr) {
+  let array = arr.filter((notZeros) => notZeros !== 0)
+  const zerosArr = arr.filter((zeros) => zeros === 0)
+  arr.filter.push(...zerosArr)
+  return array
+}
+
+console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
