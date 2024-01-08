79824e7e98bd53a575230079b5795bcfb0f26630
Erodri6z <erikrodriguez2417@gmail.com>
2024-01-08 17:08:12 -0600

    complete the challange

--- javascript.js
+++ javascript.js
@@ -7,3 +7,18 @@
 
 // console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
 
+// complete the function
+function solution(string) {
+  let reg = /[A-Z]/g
+  let arr = string.split("")
+  for (let i = 1;  i < arr.length; i++ ){
+    arr[i].match(reg) ? arr[i] = ` ${arr[i]}`: arr[i]
+  }
+  console.log(arr.join(""))
+
+}
+
+solution("camelCasTing")
+solution("camelCasting")
+solution("JavaScript")
+solution("INeedBurger")
