c098b93234804f227f0b5021d010cdba155f8ad6
Erodri6z <erikrodriguez2417@gmail.com>
2024-01-08 16:09:20 -0600

    reset for next challange

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
-function moveZeros(arr) {
-  let array = arr.filter((notZeros) => notZeros !== 0)
-  const zerosArr = arr.filter((zeros) => zeros === 0)
-  arr.filter.push(...zerosArr)
-  return array
-}
+// function moveZeros(arr) {
+//   let array = arr.filter((notZeros) => notZeros !== 0)
+//   const zerosArr = arr.filter((zeros) => zeros === 0)
+//   arr.filter.push(...zerosArr)
+//   return array
+// }
 
-console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
+// console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
+
