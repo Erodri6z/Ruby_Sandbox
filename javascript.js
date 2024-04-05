56e7971dd913f6d171eee0f698f96c51c403f553
Erodri6z <erikrodriguez2417@gmail.com>
2024-04-04 20:19:20 -0500

    add a new challange

--- javascript.js
+++ javascript.js
@@ -188,3 +188,12 @@
 
 // getCount("abracadabra")
 // getCount("amongus")
+
+function comp(ar1, ar2) {
+
+}
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+comp(a, b)
