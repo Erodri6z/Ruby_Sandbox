687de5fca106414534de9fa0b7208636b74861ae
Erodri6z <erikrodriguez2417@gmail.com>
2024-06-10 18:57:21 -0500

    comment out code and prepare for the next challenge

--- javascript.js
+++ javascript.js
@@ -315,25 +315,25 @@
 // isTriangle(7,2,2)
 // isTriangle(1,2,2)
 
-function count(str){
-  let object = {}
-  for (let i = 0; i < str.length; i++) {
-    let count = 0
-    for( let j = 0 ;j < str.length; j++){
-      if( str[i] == str[j] && i > j  ) {
-        break
-      }
-      if( str[i] == str[j]  ) {
-        count++
-      }
-    }
-    if( count > 0 ){
-      if(!object[str[i]]){
-        object[str[i]] = count
-      }
-    }
-  }
-  console.log(object)
-}
+// function count(str){
+//   let object = {}
+//   for (let i = 0; i < str.length; i++) {
+//     let count = 0
+//     for( let j = 0 ;j < str.length; j++){
+//       if( str[i] == str[j] && i > j  ) {
+//         break
+//       }
+//       if( str[i] == str[j]  ) {
+//         count++
+//       }
+//     }
+//     if( count > 0 ){
+//       if(!object[str[i]]){
+//         object[str[i]] = count
+//       }
+//     }
+//   }
+//   console.log(object)
+// }
 
-count('abaabaabb')
+// count('abaabaabb')
