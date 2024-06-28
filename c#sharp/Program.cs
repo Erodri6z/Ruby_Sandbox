d54b54c20f9fd86f567e0a8535218941b59c82e9
Erodri6z <erikrodriguez2417@gmail.com>
2024-06-27 20:30:18 -0500

    remove the coding challange and sprepare for the next one

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -2,19 +2,19 @@
 
 public class Program
 {
-    public static int SumOfIntegers(params int[] numbers)
-    {
-        int sum = 0;
-        foreach (int number in numbers)
-        {
-            sum += (number * number);
-        }
-        return sum;
-    }
+    // public static int SumOfIntegers(params int[] numbers)
+    // {
+    //     int sum = 0;
+    //     foreach (int number in numbers)
+    //     {
+    //         sum += (number * number);
+    //     }
+    //     return sum;
+    // }
 
     public static void Main()
-    {
-        int result = SumOfIntegers(5,3,4);
-        Console.WriteLine(result);  
-    }
+    // {
+    //     int result = SumOfIntegers(5,3,4);
+    //     Console.WriteLine(result);  
+    // }
 }
