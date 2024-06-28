e0c1acf85c58fe319fe035ad290d980ed0c6877b
Erodri6z <erikrodriguez2417@gmail.com>
2024-06-27 21:01:31 -0500

    remove previous and start a new

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -2,19 +2,19 @@
 
 public class Program
 {
-    public static double SumArray(params double[] array)
-    {
-      double sum = 0;
-      foreach(double n in array)
-      {
-        sum += n;
-      }
-      return sum;
-    }
+    // public static double SumArray(params double[] array)
+    // {
+    //   double sum = 0;
+    //   foreach(double n in array)
+    //   {
+    //     sum += n;
+    //   }
+    //   return sum;
+    // }
 
     public static void Main()
     {
-      double result = SumArray(2,3,4);
-      Console.WriteLine(result);    
+      // double result = SumArray(2,3,4);
+      // Console.WriteLine(result);    
     }
 }
