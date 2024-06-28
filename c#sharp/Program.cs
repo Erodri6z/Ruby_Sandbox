e3c41707f2ecf7e768c54bdf073b5bb1b5af5dc7
Erodri6z <erikrodriguez2417@gmail.com>
2024-06-27 20:29:04 -0500

    add the first coding challange that is in c# and solve

---
+++ c#sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static int SumOfIntegers(params int[] numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += (number * number);
+        }
+        return sum;
+    }
+
+    public static void Main()
+    {
+        int result = SumOfIntegers(5,3,4);
+        Console.WriteLine(result);  
+    }
+}
--- c-sharp.cs
+++ c-sharp.cs
@@ -2,6 +2,16 @@ public static class Kata
 {
   public static int SquareSum(int[] numbers)
   { 
-    
+    int sum = 0
+    foreach (int n in numbers)
+    {
+      sum += n
+    }
+    return sum
+  }
+  public static void Main()
+  {
+    int result = SquareSum(1,2,3)
+    Console.WriteLine(result)
   }
 }
