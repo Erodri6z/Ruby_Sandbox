15248d7a252e9ccda31c8e47e45e6f5f84c49966
Erodri6z <erikrodriguez2417@gmail.com>
2024-06-30 13:27:29 -0500

    remove the coding chal;lange and prep for the next one

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -2,23 +2,23 @@
 
 public class Program
 {
-  public static string BooleanToString(bool b)
-  {
-    if (b == true)
-    {
-      return "True";
-    }
-    else
-    {
-      return "False";
-    }
-  }
+  // public static string BooleanToString(bool b)
+  // {
+  //   if (b == true)
+  //   {
+  //     return "True";
+  //   }
+  //   else
+  //   {
+  //     return "False";
+  //   }
+  // }
 
     public static void Main()
     {
-      string result = BooleanToString(true);
-      string result2 = BooleanToString(false);
+      // string result = BooleanToString(true);
+      // string result2 = BooleanToString(false);
       Console.WriteLine(result);
-      Console.WriteLine(result2);    
+      // Console.WriteLine(result2);    
     }
 }
