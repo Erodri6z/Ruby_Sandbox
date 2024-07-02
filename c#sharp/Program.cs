761f1797960b6fcd6ebf129489ed63428b7616d6
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-01 19:10:13 -0500

    remove the coding challange in c#

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-using System.Text.RegularExpressions;
+// using System.Text.RegularExpressions;
 
 public class Program
 {
-  public static string PrinterError(string s)
-  { 
-    string pattern = "[^abcdefghijklm]";
-    Regex reg = new Regex(pattern);
-    int errorCount = reg.Matches(s).Count;
-    return $"{errorCount}/{s.Length}";
-  }
+//   public static string PrinterError(string s)
+//   { 
+//     string pattern = "[^abcdefghijklm]";
+//     Regex reg = new Regex(pattern);
+//     int errorCount = reg.Matches(s).Count;
+//     return $"{errorCount}/{s.Length}";
+//   }
   public static void Main()
     {
-      string result = PrinterError("aaabbbbhaijjjm");
+      // string result = PrinterError("aaabbbbhaijjjm");
       Console.WriteLine(result);
     }
 }
