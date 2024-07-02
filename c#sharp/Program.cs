4c3be700cada42669e3e21811a87c476428e9119
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-01 19:09:02 -0500

    add the same coding challanges solution in c#

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 public class Program
 {
   public static string PrinterError(string s)
   { 
-    string reg = /[abcdefghijklm]/gi;
-    return {s.Replace(reg, '').Length} + '/' + {s.Length};
+    string pattern = "[^abcdefghijklm]";
+    Regex reg = new Regex(pattern);
+    int errorCount = reg.Matches(s).Count;
+    return $"{errorCount}/{s.Length}";
   }
   public static void Main()
     {
-    string result = PrinterError("aaabbbbhaijjjm");
+      string result = PrinterError("aaabbbbhaijjjm");
       Console.WriteLine(result);
     }
 }
--- c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
+++ c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b1a946c9f510ca35ce975d169d593920177896c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17a13bc8a4a557f7d495e8fe5825441f43747137")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
