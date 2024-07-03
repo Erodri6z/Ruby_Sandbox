25ae09008992ed5e7270df3d23edeba03ed430f1
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-02 19:57:41 -0500

    add a solution to the coding challange

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -5,10 +5,18 @@ public class Program
 {
   public static int PaperWork(int n, int m)
   {
+    if ( n< 0 || m < 0)
+    {
+      return 0;
+    } 
+    else
+    {
+      return n * m;
+    }
   }
   public static void Main()
     {
-      int result = PaperWork(3, 2);
+      int result = PaperWork(5, -5);
       Console.WriteLine(result);
     }
 }
