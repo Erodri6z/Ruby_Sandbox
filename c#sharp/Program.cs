fa90adb86af62300736f455d7a94bbb803b0d2c8
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-07 14:07:10 -0500

    add a solution

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -1,20 +1,23 @@
 ﻿using System;
+// using System.Collections.Generic;
 // using System.Text.RegularExpressions;
 
 public class Program
 {
   public static int[] CountBy(int x, int n)
   {
-    int[] z = new int[] {
-      
-    };
-    
+    int[] z = new int[n];
+
+    for (int count = 0; count < n; count++)
+    {
+      z[count] = x * (count + 1);
+    }
     
     return z;
   }
   public static void Main()
     {
       int[] result = CountBy(2,10);
-      Console.WriteLine(result);
+      Console.WriteLine(string.Join(".", result));
     }
 }
--- c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
+++ c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+065c9500ecc3cd1eb5280b1a716f4ba5c48d18fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4952ca4caa27e9aa244104c25e162d910064ca87")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
