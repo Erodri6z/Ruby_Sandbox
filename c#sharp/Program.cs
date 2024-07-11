620bd08234d1c6a420b4505d25fa107665c081c4
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-10 23:25:24 -0500

    add a solution to the code

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -6,11 +6,20 @@ public class Program
 {
   public static string AreYouPlayingBanjo(string name)
   {
+    string firstChar = name.Substring(0, 1);
+    if (firstChar.ToUpper() == "R")
+    {
+      return "Plays Banjo";
+    }
+    else 
+    {
+      return "Does not play banjo";
+    }
 
   }
   public static void Main()
     {
-      string result = AreYouPlayingBanjo("Martin")
+      string result = AreYouPlayingBanjo("bryan");
       Console.WriteLine(result);
     }
 }
--- c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
+++ c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4952ca4caa27e9aa244104c25e162d910064ca87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39aedfe8df9e961b4596938d464bfa0d7cd3593c")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
