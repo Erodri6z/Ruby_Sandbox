4481600d5dc7b8224620440846fd3224c12f3ed5
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-11 18:57:02 -0500

    add a new challange

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -4,16 +4,13 @@
 
 public class Program
 {
-    public static int Past(int h, int m, int s)
+    public static double basicOp(char operation, double value1, double value2)
     {
-      int minutes = 60000;
-      int seconds = 1000;
-      int hours = 3600000;
-      return (h * hours) + (minutes * m) + (s * seconds);
+      return -1;
     }
   public static void Main()
     {
-      int result = Past(0,1,1);
+    double result = basicOp("+", 2, 4);
       Console.WriteLine(result);
     }
 }
