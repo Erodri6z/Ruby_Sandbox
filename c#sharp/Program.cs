f03ac441db8a41a847ee726116ec953a123c9a8d
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-10 23:31:14 -0500

    remove the code and prepapre for the next challange

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -4,22 +4,22 @@
 
 public class Program
 {
-  public static string AreYouPlayingBanjo(string name)
-  {
-    string firstChar = name.Substring(0, 1);
-    if (firstChar.ToUpper() == "R")
-    {
-      return name + " plays Banjo";
-    }
-    else 
-    {
-      return name + " does not play banjo";
-    }
+  // public static string AreYouPlayingBanjo(string name)
+  // {
+  //   string firstChar = name.Substring(0, 1);
+  //   if (firstChar.ToUpper() == "R")
+  //   {
+  //     return name + " plays Banjo";
+  //   }
+  //   else 
+  //   {
+  //     return name + " does not play banjo";
+  //   }
 
-  }
+  // }
   public static void Main()
     {
-      string result = AreYouPlayingBanjo("ryan");
-      Console.WriteLine(result);
+      // string result = AreYouPlayingBanjo("ryan");
+      Console.WriteLine();
     }
 }
