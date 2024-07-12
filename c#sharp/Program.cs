d360ab949ab0fa0751401916e543122510e3eb4a
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-11 19:50:14 -0500

    add a solution

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -6,11 +6,19 @@ public class Program
 {
   public static bool ValidatePin(string pin)
   {
-    return true || false;
+    int x = Convert.ToInt16(pin);
+    if (x.ToString().Count() == 4 || x.ToString().Count() == 6)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
   public static void Main()
     {
-    bool result = ValidatePin("1234");
+    bool result = ValidatePin("21234");
       Console.WriteLine(result);
     }
 }
