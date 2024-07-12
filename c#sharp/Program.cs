6db6ab2c407d96866b833cfce1121fa801520501
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-11 20:26:04 -0500

    remove the coding challange and prepare for the next one

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -4,24 +4,24 @@
 
 public class Program
 {
-  public static bool ValidatePin(string pin)
-  {
-    if (pin.Length == 4 || pin.Length == 6)
-    {
-      foreach( char c in pin)
-      {
-        if (!char.IsDigit(c))
-        {
-          return false;
-        }
-      }
-      return true;
-    }
-    return true;
-  }
+  // public static bool ValidatePin(string pin)
+  // {
+  //   if (pin.Length == 4 || pin.Length == 6)
+  //   {
+  //     foreach( char c in pin)
+  //     {
+  //       if (!char.IsDigit(c))
+  //       {
+  //         return false;
+  //       }
+  //     }
+  //     return true;
+  //   }
+  //   return false;
+  // }
   public static void Main()
     {
-    bool result = ValidatePin("21234");
-      Console.WriteLine(result);
+    // bool result = ValidatePin("21234");
+      Console.WriteLine("result");
     }
 }
