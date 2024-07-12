219f3937660036a16a5bad22e4661c8f9dcee9e4
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-11 19:24:37 -0500

    remove the coding challange

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -4,32 +4,32 @@
 
 public class Program
 {
-    public static double BasicOp(char operation, double value1, double value2)
-    {
-      if (operation == '+')
-      {
-        return value1 + value2;
-      }
-      else if (operation == '-')
-      {
-        return value1 - value2;
-      }
-      else if (operation == '*')
-      {
-        return value1 * value2;
-      }
-      else if (operation == '/')
-      {
-        return value1 / value2;
-      }
-      else
-      {
-        return -1;
-      }
-    }
+    // public static double BasicOp(char operation, double value1, double value2)
+    // {
+    //   if (operation == '+')
+    //   {
+    //     return value1 + value2;
+    //   }
+    //   else if (operation == '-')
+    //   {
+    //     return value1 - value2;
+    //   }
+    //   else if (operation == '*')
+    //   {
+    //     return value1 * value2;
+    //   }
+    //   else if (operation == '/')
+    //   {
+    //     return value1 / value2;
+    //   }
+    //   else
+    //   {
+    //     return -1;
+    //   }
+    // }
   public static void Main()
     {
-    double result = BasicOp('*', 2, 4);
+    // double result = BasicOp('*', 2, 4);
       Console.WriteLine(result);
     }
 }
