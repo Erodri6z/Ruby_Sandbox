a681d9e6b1cae45f989763cff0d2b16e6d337845
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-16 22:36:50 -0500

    add a new coding challange

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -4,37 +4,15 @@
 
 public class Program
 {
-  public static char GetGrade(int s1, int s2, int s3)
+  public static bool BetterThanAverage(int[] ClassPoints, int YourPoints)
   {
-    int average = (s1 + s2 + s3)/ 3;
-    char grade = 'A';
-
-    if (average >= 90) 
-    {
-      grade = 'A';
-    }
-    else if (average >= 80)
-    {
-      grade = 'B';
-    }
-    else if (average >= 70)
-    {
-      grade = 'C';
-    }
-    else if (average >= 60)
-    {
-      grade = 'D';
-    } 
-    else
-    {
-      grade = 'F';
-    }
-
-    return grade;
+    //Insert brain here
+    return false;
+    
   }
   public static void Main()
     {
-      char result = GetGrade(93, 90, 93);
+      bool result = BetterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75);
       Console.WriteLine(result);
     }
 } 
