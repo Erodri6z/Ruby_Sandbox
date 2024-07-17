2c025008d1bf1ae56566d987ae434eeffb5c786e
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-17 18:32:39 -0500

    add a solution

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -5,14 +5,14 @@
 
 public class Program
 {
-    public static int CountSheeps(bool[] sheeps)
-  {
-    
-    
-  }
+  public static int CountSheeps(bool[] sheeps)
+    {
+      bool[] present = Array.FindAll(sheeps, c => c == true);
+      return present.Count();
+    }
   public static void Main()
     {
-      int result = CountSheeps(true, false, true);
+      int result = CountSheeps([true, false, true]);
       Console.WriteLine(result);
     }
 } 
--- c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
+++ c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62f418e541b445c754596e072243486e791eee3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5adc5f70cbbd7ffb672668a89bef9103328c19b")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
