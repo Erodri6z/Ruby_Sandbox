d6ea929064e2b6bb71e4883f53a40eefe1fc455b
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-17 14:23:58 -0500

    add a solution to the coding challange

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -8,11 +8,25 @@ public class Program
   public static string GetMiddle(string s)
   {
   //Code goes here!
-  
+  bool even = s.Count() % 2 == 0;
+  int middle = s.Count() / 2;
+  Console.WriteLine(even);
+
+  if (even)
+  {
+    Console.WriteLine("even");
+    return s.Substring(middle - 1, 2);
+  }
+  else
+  {
+    Console.WriteLine("Odd");
+    return s.Substring(middle, 1 );
+  }
+
   }
   public static void Main()
     {
-      string result = GetMiddle("Man");
+      string result = GetMiddle("Manma");
       Console.WriteLine(result);
     }
 } 
