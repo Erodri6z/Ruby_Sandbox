a355576075a9800ed9261aedade982039d6324ce
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-20 19:09:30 -0500

    add a new coding challange

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -5,22 +5,13 @@
 
 public class Program
 {
-  public static int NbYear(int p0, double percent, int aug, int p) 
+	public static string Longest(string s1, string s2) 
   {
     // your code
-    double percentage = percent / 100;
-    int count = 0;
-    while (p0 < p)
-    {
-       p0 = (int)(p0 + (p0 * percentage) + aug);
-      count++;
-    }
-    return count;
   }
   public static void Main()
     {
-      int result = NbYear(1500000, 0.25, 1000, 2000000);
+      string result = Longest("aretheyhere", "yestheyarehere");
       Console.WriteLine(result);
     }
 } 
-
