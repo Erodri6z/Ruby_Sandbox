c0ffd7f8f6ac7d97206f2468c8d3ec976f7c1344
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-24 21:15:25 -0500

    add a solution

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 // using System.Linq;
-// using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 // using System.Text.RegularExpressions;
 
 public class Program
 {
   public static List<string> Number(List<string> lines) 
   {
-      //your code goes here
-      return lines;
+    List<string> result = new List<string>();
+
+    for (int i = 0; i < lines.Count; i++)
+    {
+      result.Add($"{i + 1}: {lines[i]}");
+    }
+
+
+    return result;
   }
   public static void Main()
     {
-    List<string> results = Number(["A", "B", "C"]);
-      Console.WriteLine(results);
+      List<string> results = Number(["A", "B", "C"]);
+      Console.WriteLine(results[1]);
     }
 } 
--- c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
+++ c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+402306af587a3dd4e479ce6a3749beda4a2f0e9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc2fb2458f9e6296486b7dc9da1a126671800ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
