60ae9ab3562573827aa3a74513042803b68f5709
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-27 10:29:25 -0500

    add a new coding challange that is actually a challange

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 // using System.Linq;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
+// using System.Collections.Generic;
+// using System.Runtime.InteropServices;
 // using System.Text.RegularExpressions;
 
 public class Program
 {
-  public static string MakeUpperCase(string str)
-  {
-    return str.ToUpper();
-  }
+    public static string ExpandedForm(long num) 
+    {
+      throw new NotImplementedException();
+    }
   public static void Main()
     {
-      string result = MakeUpperCase("aaaaaaa");
+      string result = ExpandedForm(2030);
       Console.WriteLine(result);
     }
 } 
