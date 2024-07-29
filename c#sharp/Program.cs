cdda9405320d22ed45668a8ef59b859a435d6ad0
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-28 19:00:32 -0500

    remove the commented out code so that I can send it tin codewars

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -9,27 +9,6 @@ public class Program
     public static string ExpandedForm(long num) 
     {
       int[] digits = num.ToString().Select(o => Convert.ToInt32(o) - 48).ToArray();
-      // int[] mainDigits = digits.Where(c => c != 0).ToArray();
-      // List<int> idx = new List<int>(); 
-
-      // for (int i = 0; i < digits.Length; i++)
-      // {
-      //   if (mainDigits.Contains(digits[i]))
-      //   {
-      //     idx.Add(i);
-      //   }
-      // }
-
-      // int[] idxArray = idx.ToArray();
-
-      // string result = "";
-
-      // for (int y = 0; y < mainDigits.Length; y++ )
-      // {
-      //   int position = Array.IndexOf(digits, mainDigits[y]);
-      //   result = result + $" + {mainDigits[y]}{new String('0', digits.Length - position - 1)}";
-      // }  
-      // return result;
       string result = "";
 
       for (int i = 0; i < digits.Length; i++)
