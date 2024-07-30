0bf65b039e3c1f04eb5ba651504db874bb4d9982
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-29 20:23:35 -0500

    refactor the code, now its more fool proof

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -9,14 +9,30 @@ public class Program
 {
   public static string Rgb(int r, int g, int b) 
   {
-    string red = r.ToString("X2");
-    string green = g.ToString("X2");
-    string blue = b.ToString("X2");
-    return "#" + red + green + blue;
+    static string ConvertToHex( int x )
+    {
+      if ( x < 0 )
+      {
+        return "00";
+      }
+      else if ( x > 255 )
+      {
+        return "FF";
+      }
+      else
+      {
+        return x.ToString("X2");
+      }
+    }
+    string red = ConvertToHex(r);
+    string green = ConvertToHex(g);
+    string blue = ConvertToHex(b);
+
+    return "#" + red + green + blue;  
   }
   public static void Main()
     {
-      string result = Rgb(225, 165, 0);
+      string result = Rgb(255, 255, 300);
       Console.WriteLine(result);
     }
 } 
