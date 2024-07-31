ea03d476de65da9d45149e4750f7e7a7cbd25f60
Erodri6z <erikrodriguez2417@gmail.com>
2024-07-31 17:42:30 -0500

    add a solution

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -9,11 +9,27 @@ public class Program
 {
   public static int[] TwoSum(int[] nums, int target)
   {
-    return nums;
+    int a = 0;
+    int b = 0;
+
+    
+    for (int i = 0; i < nums.Length; i++ )
+    {
+      for ( int j = 0; j < nums.Length; j++)
+      {
+        if (nums[i] + nums[j] == target)
+        {
+          a = nums[i];
+          b = nums[j];
+        }
+      } 
+    }
+
+    return [a, b];
   }
   public static void Main()
     {
       int[] result = TwoSum([1,2,3], 4);
-      Console.WriteLine(result);
+      Console.WriteLine($"{result[0]}, {result[1]}");
     }
 } 
--- c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
+++ c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e91ea9f9ad932ccfc5a66964da26e08d00dd0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc697523e64e8a55da759a745b7fc838109bcfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
