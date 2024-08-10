e5e4d92c652195e81104f69e7772a13f4ce698ab
Erodri6z <erikrodriguez2417@gmail.com>
2024-08-10 14:51:44 -0500

    remove the coding challange

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -10,22 +10,10 @@ using System.Runtime.InteropServices;
 
 public class Program
 {
-  public static string CreatePhoneNumber(int[] numbers)
-  {
-    string number = string.Join("", numbers);
-    if (numbers.Length >= 10)
-    {
-      string v = $"({number.Substring(0, 3)}) {number.Substring(3, 3)}-{number.Substring(6, 4)}";
-      return v;
-    }
-    else
-    {
-      return "error";
-    }
-  }
+
   public static void Main()
     {
-      string result = CreatePhoneNumber(new int[] {3, 1, 2, 6, 5, 4, 7, 8, 9, 0});
+      // string result = CreatePhoneNumber(new int[] {3, 1, 2, 6, 5, 4, 7, 8, 9, 0});
       Console.WriteLine(result);
     }
 } 
