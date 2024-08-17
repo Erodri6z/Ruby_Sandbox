5886f518fdeda6b003db56f935e25de1b3451b89
Erodri6z <erikrodriguez2417@gmail.com>
2024-08-16 21:19:46 -0500

    add a solution

--- c#sharp/Program.cs
+++ c#sharp/Program.cs
@@ -12,11 +12,25 @@ public class Program
 {
   public static char FindMissingLetter(char[] array)
   {
+    string alpha = "abcdefghijklmnopqrstuvwxyz";
+    bool isUpperCase = char.IsUpper(array[0]);
+    alpha = isUpperCase ? alpha.ToUpper() : alpha;
+
+    int start = alpha.IndexOf(array[0]);
+
+    // Console.WriteLine(start);
+    for (int i = start; i < alpha.Length; i++) 
+    {
+      if (!array.Contains(alpha[i]))
+      {
+        return alpha[i];
+      }
+    }
     return ' ';
   }
   public static void Main()
     {
-      char result = FindMissingLetter(['a','c']);
+      char result = FindMissingLetter(['B','C','E']);
       Console.WriteLine(result);
     }
 } 
--- c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
+++ c#sharp/obj/Debug/net8.0/c#sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431d53c1855bb6d877ca695c55f63d47173591c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99172e735764509b0f8232820169ef73dadd0425")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
