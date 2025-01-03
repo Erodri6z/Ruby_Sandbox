91d4ee1d6c4b580067f407f2115da60e0757fb1b
Erodri6z <erikrodriguez2417@gmail.com>
2025-01-02 19:40:03 -0600

    remove code

--- javascript.js
+++ javascript.js
@@ -607,27 +607,27 @@
 // order("")
 
 
-function triangle(row) {
-  let alpha = "abcdefghijklmnopqrstuvwxyz".split("")
-  let rowArr = row.split("") 
+// function triangle(row) {
+//   let alpha = "abcdefghijklmnopqrstuvwxyz".split("")
+//   let rowArr = row.split("") 
   
   
-  while (rowArr.length !== 1) {
-    let arr = []
-    console.log(rowArr)
-    for (let i = 0; i < rowArr.length - 1; i++){
-      let num = alpha.indexOf(rowArr[i]) + alpha.indexOf(rowArr[i + 1]) + 1
-      if (num >= 26) {
-        num -= 26
-      }
-      arr.push(alpha[num])
-    }
-    rowArr = arr
-  }
-  console.log(rowArr[0])
-}
+//   while (rowArr.length !== 1) {
+//     let arr = []
+//     console.log(rowArr)
+//     for (let i = 0; i < rowArr.length - 1; i++){
+//       let num = alpha.indexOf(rowArr[i]) + alpha.indexOf(rowArr[i + 1]) + 1
+//       if (num >= 26) {
+//         num -= 26
+//       }
+//       arr.push(alpha[num])
+//     }
+//     rowArr = arr
+//   }
+//   console.log(rowArr[0])
+// }
 
-// triangle('codewars')
-// triangle('youhavechosentotranslatethiskata')
-triangle('triangle')
-// triangle('b')
+// // triangle('codewars')
+// // triangle('youhavechosentotranslatethiskata')
+// triangle('triangle')
+// // triangle('b')
